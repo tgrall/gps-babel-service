ba0053752b931ad2f392cf3e223f7d31db2f6c9e
tgrall <tugdual@gmail.com>
2023-01-22 17:53:05 +0100

    move to type script

--- build/Services/TraceConverter.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.convertTrace = void 0;
-const convertTrace = (params) => {
-    return "OK";
-};
-exports.convertTrace = convertTrace;
